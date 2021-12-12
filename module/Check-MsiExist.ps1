9ba752548a67ab2f021f19d243aca6cccbd03f47
pzohaycuoi <pzohaycuoi@gmail.com>
2021-12-13 01:36:05 +0700

    change result order Check-MsiExist.ps1

--- module/Check-MsiExist.ps1
+++ module/Check-MsiExist.ps1
@@ -10,15 +10,15 @@ function Check-MsiExist {
   # check if the provided MSI exist in the resource group
   try {
     $result = [PSCustomObject]@{}
+    $result | Add-Member -NotePropertyName "MsiName" -NotePropertyValue $msiName
     $getMsi = Get-AzUserAssignedIdentity -ResourceGroupName $ResourceGroup -Name $MsiName -ErrorAction Stop
-    $result | Add-Member -NotePropertyName "MsiName" -NotePropertyValue $msiNameInRg
   
     if ($getMsi.Name -contains $MsiName) {      
       $result | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
       return $result
     }
     else {    
-      $result | Add-Member -NotePropertyName "Log" -NotePropertyValue "Failed: Msi $($msiNameInRg) not exist"
+      $result | Add-Member -NotePropertyName "Log" -NotePropertyValue "Failed: Msi $($msiName) not exist"
       $result | Add-Member -NotePropertyName "Result" -NotePropertyValue $false
       return $result
     }
