37880acab96cbe638d5f2e8393fb0ca72203af59
pzohaycuoi <pzohaycuoi@gmail.com>
2021-12-13 11:44:10 +0700

    change function and file name according to approved verb

--- common/Test-PathIsLinuxOrWin.ps1
+++ common/Test-PathIsLinuxOrWin.ps1
@@ -1,5 +1,4 @@
-function Check-PathIsLinuxOrWin {
-
+function Test-PathIsLinuxOrWin {
   param (
     [Parameter(Mandatory)]
     [string]$FilePath
@@ -7,19 +6,13 @@ function Check-PathIsLinuxOrWin {
 
   # Check if path type is linux or windows then change $FilePath accordingly
 
-  $getCurrentPath = (pwd).Path
-
-  # if current path contain \ then it's Windows, / then it's Linux
-  if ($getCurrentPath -clike "*\*") {
-    
+  $getCurrentPath = (pwd).Path  
+  
+  if ($getCurrentPath -clike "*\*") {    
     $FilePath = $FilePath.Replace("/","\")
     return $FilePath
-
   } elseif ($getCurrentPath -clike "*/*") {
-
     $FilePath = $FilePath.Replace("\","/")
     return $FilePath
-
-  } # end if ($getCurrentPath -clike "*\*")
-  
-} # end function Check-PathIsLinuxOrWin
+  } 
+}
